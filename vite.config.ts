c54c6ebd636cf1a66381bd62c58fa3aba4f81c14
Ahmed Afrid <77157472+ahmedafrid@users.noreply.github.com>
2025-11-14 17:10:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // IMPORTANT for GitHub Pages:
-  // on production builds, use the repo name as base
+  // 👇 THIS IS THE IMPORTANT PART
   base: mode === "development" ? "/" : "/portfolio/",
 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
